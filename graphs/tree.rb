8346eb6e19ea6f91198facc7e088bc19641999c9
Samuel Fahlberg <sam.fahlberg@gmail.com>
2015-02-22 12:51:46 -0800

    add tree data structure

---
+++ graphs/tree.rb
@@ -0,0 +1,22 @@
+class Node
+	attr_accessor :parent, :children, :value
+
+	def initialize(value)
+		@value = value
+		@parent = nil
+		@children = []
+	end
+
+	def set_parent(parent_node)
+		if @parent
+			@parent.children.delete(self)
+		end
+
+		@parent = parent_node
+		@parent.children << self 
+	end
+
+	def add_child(child_node)
+		child_node.set_parent(self)
+	end
+end
