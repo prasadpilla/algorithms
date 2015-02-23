46112a980c42cc992be6f23cbe10125233abfc3b
Samuel Fahlberg <sam.fahlberg@gmail.com>
2015-02-23 15:32:17 -0800

    add dfs recursive

--- graphs/dfs.rb
+++ graphs/dfs.rb
@@ -11,4 +11,14 @@ class Node
 			nodes_to_check.concat(current_node.children)
 		end
 	end
+
+	def dfs_recursive(target)
+		return self if self.value == target
+
+		self.children.each do |child|
+			response = child.dfs_recursive(target)
+			return response if !response.nil?
+		end
+		nil
+	end
 end
--- spec/graphs/test-dfs.rb
+++ spec/graphs/test-dfs.rb
@@ -1,24 +1,52 @@
 require 'rspec'
 require_relative '../../graphs/dfs'
 
-describe 'dfs_iterative' do
-	it 'finds a node that exists' do
-		a = Node.new(1)
-		b = Node.new(2)
-		c = Node.new(3)
+describe 'dfs' do
+	describe 'dfs_iterative' do
+		it 'finds a node that exists' do
+			a = Node.new(1)
+			b = Node.new(2)
+			c = Node.new(3)
 
-		a.add_child(b)
-		a.add_child(c)
+			a.add_child(b)
+			a.add_child(c)
 
-		expect(a.dfs_iterative(2)).to eq(b)
+			expect(a.dfs_iterative(2)).to eq(b)
+		end
+
+		it 'returns nil when no node exists' do
+			a = Node.new(1)
+			b = Node.new(2)
+
+			a.add_child(b)
+
+			expect(a.dfs_iterative(3)).to be_nil
+		end
 	end
 
-	it 'returns nil when no node exists' do
-		a = Node.new(1)
-		b = Node.new(2)
+	describe 'dfs_recursive' do
+		it 'finds a node that exists' do
+			a = Node.new(1)
+			b = Node.new(2)
+			c = Node.new(3)
+			d = Node.new(4)
+			e = Node.new(5)
 
-		a.add_child(b)
+			a.add_child(b)
+			a.add_child(c)
+			b.add_child(d)
+			b.add_child(e)
 
-		expect(a.dfs_iterative(3)).to be_nil
+			expect(a.dfs_recursive(4)).to eq(d)
+		end
+
+		it 'returns nil when no node exists' do
+			a = Node.new(1)
+			b = Node.new(2)
+
+			a.add_child(b)
+
+			expect(a.dfs_recursive(3)).to be_nil
+		end
 	end
 end
